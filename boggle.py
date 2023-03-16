9f5975dbbffa63048b9a536b6998301dc814a86f
jonnyDurston <jonathan.durston@btinternet.com>
2023-03-16 23:51:40 +0000

    All in same window

--- boggle.py
+++ boggle.py
@@ -54,7 +54,7 @@ def decodeLetters(coded):
     return letters
 
 
-def drawBoard(width, height):
+def drawBoard(boggleBoard, letters, width, height):
     board_coords, border_size, die_width, padding, font_size, tile_border = get_dimensions(width, height)
     boggleBoard.create_rectangle(board_coords, fill = "#FF6600", width=0)
     index = 0
@@ -96,54 +96,88 @@ def get_dimensions(width, height):
     return board_coords, border_size, die_width, padding, font_size, tile_border
 
 
-def cover():
-    board_coords, border_size, die_width, padding, font_size, tile_border = get_dimensions(root.winfo_width(), root.winfo_height())
+def cover(boggleBoard):
+    print("here")
+    board_coords, border_size, die_width, padding, font_size, tile_border = get_dimensions(window.winfo_width(), window.winfo_height())
     c = boggleBoard.create_rectangle(board_coords, fill="#BBBBBB", tags="rem")
     t = boggleBoard.create_text(
-        root.winfo_width()//2, root.winfo_height()//2, tags="rem", text="Click to reveal", font=("Bahnschrift", font_size)
+        window.winfo_width()//2, window.winfo_height()//2, tags="rem", text="Click to reveal", font=("Bahnschrift", font_size)
     )
     boggleBoard.tag_bind("rem", "<ButtonPress-1>", lambda x: boggleBoard.delete("rem"))
     boggleBoard.pack(fill="both", expand=True)
 
 
-def alarm():
-    board_coords, border_size, die_width, padding, font_size, tile_border = get_dimensions(root.winfo_width(), root.winfo_height())
+def alarm(boggleBoard):
+    board_coords, border_size, die_width, padding, font_size, tile_border = get_dimensions(window.winfo_width(), window.winfo_height())
     a = boggleBoard.create_text(
-        min(root.winfo_width(), root.winfo_height()), min(root.winfo_width(), root.winfo_height()) * 9 / 10, text="30 seconds left!", font=("Bahnschrift", font_size//2), fill="white"
+        min(window.winfo_width(), window.winfo_height()), min(window.winfo_width(), window.winfo_height()) * 9 / 10, text="30 seconds left!", font=("Bahnschrift", font_size//2), fill="white"
     )
-    root.after(5000, lambda: boggleBoard.delete(a))
+    window.after(5000, lambda: boggleBoard.delete(a))
 
 
 def resize_start_window(event, title, firstTime):
     if firstTime:
-        startWindow.after(10, lambda: resize_start_window(event, title, False))
-    elif event.width == startWindow.winfo_width() and event.height == startWindow.winfo_height():
+        window.after(10, lambda: resize_start_window(event, title, False))
+    elif event.width == window.winfo_width() and event.height == window.winfo_height():
         title.config(font=("Berlin Sans FB Demi", (event.width + event.height) // 15))
 
 
-def resize_board(event, board, firstTime):
+def resize_board(event, board, letters, firstTime):
     if firstTime:
-        root.after(10, lambda: resize_board(event, board, False))
-    elif event.width == root.winfo_width() and event.height == root.winfo_height():
-        boggleBoard.delete("all")
-        drawBoard(event.width, event.height)
+        window.after(10, lambda: resize_board(event, board, letters, False))
+    elif event.width == window.winfo_width() and event.height == window.winfo_height():
+        board.delete("all")
+        drawBoard(board, letters, event.width, event.height)
+
+
+def clear():
+    list = window.pack_slaves()
+    for l in list:
+        l.destroy()
+
+
+def playGame():
+    window.title("Boggle Board")
+    window.configure(bg="#FF6600")
+
+    clear()
+
+    if seed.get() == "":
+        letters = generateLetters()
+    else:
+        letters = decodeLetters(seed.get())
+
+    boggleBoard = Canvas(
+        window,
+        bg="light blue",
+        width=700,
+        height=700
+    )
+    boggleBoard.pack(fill="both", expand=True)
+
+    window.bind("<Configure>", lambda x: resize_board(x, boggleBoard, letters, True))
+
+    window.after(180 * 1000, lambda: cover(boggleBoard))
+
+    window.after(150 * 1000, lambda: alarm(boggleBoard))
+
         
 
 
 # Welcome
-startWindow = Tk()
-startWindow.title("Boggle")
-startWindow.minsize(500, 500)
-startWindow.configure(bg="Light blue")
+window = Tk()
+window.title("Boggle")
+window.minsize(500, 500)
+window.configure(bg="Light blue")
 
-title = Label(startWindow, text="Boggle", font=("Berlin Sans FB Demi", 100), bg="Light blue")
+title = Label(window, text="Boggle", font=("Berlin Sans FB Demi", 100), bg="Light blue")
 title.pack(side="top", expand=True, fill="both", pady=(90, 10))
 
 # To resize Boggle title
-startWindow.bind("<Configure>", lambda x: resize_start_window(x, title, True))
+window.bind("<Configure>", lambda x: resize_start_window(x, title, True))
 
 
-letterInput = Frame(startWindow, bg="Light blue")
+letterInput = Frame(window, bg="Light blue")
 letterInput.pack(side="top", expand=True, fill="both", pady=60)
 
 seed = StringVar()  # Text entry
@@ -164,41 +198,10 @@ generator = Button(
 generator.pack(side="right", expand=True, fill="both", pady=0, padx=(10, 50))
 
 
-start = Button(
-    startWindow, text="START", font=("Bahnschrift 60"), command=lambda: startWindow.destroy()
-)  # GUI destroys
+start = Button(window, text="START", font=("Bahnschrift 60"), command=playGame)
 start.pack(side="top", expand=True, fill="both", pady=(0, 60), padx=(250, 250))
 
-startWindow.mainloop()
+window.mainloop()
 
-
-# GUI
-root = Tk()
-root.title("Boggle Board")
-root.configure(bg="#FF6600")
-
-
-if seed.get() == "":
-    letters = generateLetters()
-else:
-    letters = decodeLetters(seed.get())
-
-boggleBoard = Canvas(
-    root,
-    bg="light blue",
-    width=700,
-    height=700
-)
-boggleBoard.pack(fill="both", expand=True)
-
-root.bind("<Configure>", lambda x: resize_board(x, boggleBoard, True))
-
-t = Timer(180.0, lambda: cover())
-t.start()
-
-a = Timer(30.0, lambda: alarm())
-a.start()
-
-root.mainloop()
 
 # print("bean awesome")
